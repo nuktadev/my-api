632d9b07ce65a03bf4814083928bd277ce1d9e6a
programmer-nur <mohammadnur5480@gmail.com>
2026-08-08 05:54:56 +0600

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-api",
+      script: "./dist/server.js",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
